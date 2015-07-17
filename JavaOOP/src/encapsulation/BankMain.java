9e080874ea2fc068d0d32c3c7b000171319e42ea
SuperClass <SuperClass>
2015-07-17 15:20:29 +0900

    2015.07.17 클래스, 추상화

--- JavaOOP/src/encapsulation/BankMain.java
+++ JavaOOP/src/encapsulation/BankMain.java
@@ -1,14 +1,24 @@
 package encapsulation;
 
+import java.util.Scanner;
+
 public class BankMain {
 	public static void main(String[] args) {
-		String bank = "", name = "";
-		int bankbookNo = 0, money = 0, pass = 0;
-		
-		
-		
-		BankBook bb = new BankBook();
-		
-		System.out.println();
+		Scanner scanner = new Scanner(System.in);
+		/*
+		 * 생성자가 가지는 기능
+		 * 1. 객체를 만드는 기능
+		 * 2. setter 기능
+		 * 3. 객체가 만들어지는 데 필수적인 요소가 주어지지 않으면 아에 생성을 막는 기능
+		 * */
+		System.out.println("통장주 이름을 입력하세요 : ");
+		BankBook bankBook = new BankBook(scanner.next());
+		System.out.println("입금할 금액을 입력하세요 : ");
+		bankBook.deposit(scanner.nextInt());
+		System.out.println("출금할 금액을 입력하세요 : ");
+		bankBook.withdraw(scanner.nextInt());
+		System.out.println("입금할 금액을 입력하세요 : ");
+		bankBook.deposit(scanner.nextInt());
+		System.out.println(bankBook.toString());
 	}
 }
